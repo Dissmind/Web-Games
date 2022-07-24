d9b45d8f8c5e5c30e617ce4f1caa614aba28b6e3
Dissmind <dissmind@gmail.com>
2022-07-24 17:39:44 +0200

    [ADD]: init styled components

--- src/games/snake/snake.jsx
+++ src/games/snake/snake.jsx
@@ -1,8 +1,46 @@
+import styled from 'styled-components'
+
 export const Snake = () => {
 
   return (
     <div>
       <h1>Snake</h1>
+
+      <Area />
     </div>
   )
 }
+
+
+
+
+
+
+
+
+export const Area = () => {
+
+
+  return (
+    <div>
+      Area
+
+      <div>
+        <CellStl />
+      </div>
+    </div>
+  )
+}
+
+
+
+// == Styles ==
+const RowStl = styled.div`
+  display: flex;
+`
+
+const CellStl = styled.div`
+  width: 100px;
+  height: 100px;
+  background: black;
+`
