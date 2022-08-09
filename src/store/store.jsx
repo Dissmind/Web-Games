5dcbbc50544fecd6db8ca96eda4e9570f9fa51c5
Dissmind <dissmind@gmail.com>
2022-08-09 02:49:09 +0200

    [ADD](snake): redux - disabled serialization check

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,9 +1,14 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import {rootReducer} from "./root-reducer";
 
+
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false
+})
 
 export const setupStore = () => (
   configureStore({
     reducer: rootReducer,
+    middleware: customizedMiddleware
   })
 )
