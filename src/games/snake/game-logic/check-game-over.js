9ed5aa44e2645a705963de6c233b894432e617c1
Dissmind <dissmind@gmail.com>
2022-08-09 03:34:57 +0200

    [ADD](snake): check game over class

--- src/games/snake/game-logic/check-game-over.js
+++ src/games/snake/game-logic/check-game-over.js
@@ -1,23 +1,56 @@
-import { isEqualCoordinate } from '../../../pure-functions/equal-coordinates'
+import { Coordinate } from './coordinate'
+import { DefaultConfig } from '../area.slice'
+import { Serialize } from '../../../pure/serialize'
 
 
 
 export class CheckGameOver {
 
   static isCheck(snakeCoordsList) {
-    this._isCheckSmashSnake(snakeCoordsList)
+
+
+
+
+    const check = (
+      this._isCheckSmashSnake(snakeCoordsList) ||
+      this._isCheckOverflowArea(snakeCoordsList)
+    )
+
+    // TODO: remove this log
+    if (check) {
+      console.log('Game over')
+    }
+
+    return check
   }
 
 
   static _isCheckSmashSnake(snakeCoordsList) {
-    const snakeHeadCoordinate = snakeCoordsList[snakeCoordsList.length - 1]
+    const snakeHeadCoordinate = Coordinate.constructorFromObject(snakeCoordsList[snakeCoordsList.length - 1])
 
-    snakeCoordsList.forEach(i => {
-      if (isEqualCoordinate(i, snakeHeadCoordinate)) {
+    const copySnakeCoordsList = Serialize.copy(snakeCoordsList)
+    copySnakeCoordsList.pop()
+
+    copySnakeCoordsList.forEach(i => {
+      if (snakeHeadCoordinate.isEqual(i)) {
         return true
       }
     })
 
     return false
+  }
+
+
+  static _isCheckOverflowArea(snakeCoordsList) {
+    const snakeHeadCoordinate = Coordinate.constructorFromObject(snakeCoordsList[snakeCoordsList.length - 1])
+
+    const check = (
+      snakeHeadCoordinate.x < 0 ||
+      snakeHeadCoordinate.x > DefaultConfig.area.width  ||
+      snakeHeadCoordinate.y < 0 ||
+      snakeHeadCoordinate.y > DefaultConfig.area.height
+    )
+
+    return check
   }
 }
